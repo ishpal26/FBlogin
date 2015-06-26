a99b6db26a0e9329f4f929ed3cc9da7fe3feb12d
ishpal26 <ishpalsinghgill@live.co.uk>
2015-06-26 23:26:26 +0800

    first edit

---
+++ app/src/main/java/ishpal/hungrystillmustwait/LoginActivity.java
@@ -0,0 +1,55 @@
+package ishpal.hungrystillmustwait;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    public static final String FRAGMENT_TAG = "fragment_tag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        FragmentManager mFragmentManager;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = mFragmentManager.findFragmentByTag(FRAGMENT_TAG);
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        transaction.replace(android.R.id.content, new FragmentLoginPage(), FRAGMENT_TAG);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    //This method expands the menu; this adds items to the action bar if it is present.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will automatically handle clicks on the Home/Up button, as long as we specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
